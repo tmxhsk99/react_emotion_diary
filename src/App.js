f6e2a911d154501b0054283b3290f6d3009cd450
tmxhsk99 <tmxhsk99@naver.com>
2023-05-07 11:22:11 +0900

    feat : 이미지 추가 및 유틸 스크립트 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import {getEmotionImgById} from "./util";
 
 function App() {
-  return (
-    <div className="App">
-        Test
-    </div>
-  );
+    return (
+        <div className="App">
+            <img alt="감정1" src={getEmotionImgById(1)}/>
+            <img alt="감정2" src={getEmotionImgById(2)}/>
+            <img alt="감정3" src={getEmotionImgById(3)}/>
+            <img alt="감정4" src={getEmotionImgById(4)}/>
+            <img alt="감정5" src={getEmotionImgById(5)}/>
+        </div>
+    );
 }
 
 export default App;
