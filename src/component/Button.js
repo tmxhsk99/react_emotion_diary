1991e9d013dae82eb60aa9a395fea4f8f2bc980c
tmxhsk99 <tmxhsk99@naver.com>
2023-05-09 08:30:29 +0900

    feat : 전체 스타일링 적용 및 버튼 컴포넌트 구현

---
+++ src/component/Button.js
@@ -0,0 +1,26 @@
+import "./Button.css";
+const Button = ({text, type, onClick}) => {
+    let btnType = ["positive", "negative"].includes(type) ? type : "default";
+    console.log(btnType);
+    switch (btnType) {
+        case "positive":
+            btnType = "is-success";
+            break;
+        case "negative":
+            btnType = "is-error"
+            break;
+    }
+    return (
+        <button className={["nes-btn", `${btnType}`, `Button`].join(" ")} onClick={onClick}>
+            {text}
+        </button>
+    )
+}
+
+Button.defaultProps = {
+    type: "default",
+}
+
+export default Button;
+
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,33 @@
 import {useSearchParams} from "react-router-dom";
+import Button from "../component/Button";
 
 const Home = () => {
-    const [searchPrams, setSearchPrams] = useSearchParams();
-    console.log(searchPrams.get("sort"));
-    return <div>홈페이지 입니다.</div>;
+    //const [searchPrams, setSearchPrams] = useSearchParams();
+    return (
+        <div>
+            <Button
+                text={"기본 버튼"}
+                onClick={() => {
+                    alert("hi");
+                }}
+            />
+            <Button
+                type="positive"
+                text={"긍정 버튼"}
+                onClick={() => {
+                    alert("포지티브");
+                }}
+            />
+            <Button
+                type="negative"
+                text={"부정 버튼"}
+                onClick={() => {
+                    alert("부정");
+                }}
+            />
+
+        </div>
+    );
 };
 
 export default Home;
