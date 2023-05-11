3ce83a32c639b5072a53ecaf51621161320fd01d
tmxhsk99 <tmxhsk99@naver.com>
2023-05-11 14:32:28 +0900

    feat: 일기 create 구현

--- src/App.js
+++ src/App.js
@@ -4,15 +4,36 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import {Route, Routes} from "react-router-dom";
-import {useReducer} from "react";
+import {useReducer, useRef} from "react";
 
 function reducer(state, action) {
+    switch (action.type) {
+        case "CREATE" : {
+            return [action.data, ...state];
+        }
+        default: {
+            return state;
+        }
+    }
     return state;
 }
 
 function App() {
     const [data, dispatch] = useReducer(reducer, []);
+    const idRef = useRef(0);
+    const onCreate = (date, content, emotionId) =>{
+        dispatch({
+            type: "CREATE",
+            data: {
+                id: idRef.current,
+                date: new Date(date).getTime(),
+                content,
+                emotionId,
+            },
+        });
+        idRef.current += 1
 
+    }
     return (
         <div className="App">
             <Routes>
