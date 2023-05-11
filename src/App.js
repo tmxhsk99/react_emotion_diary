58728b65f3778ca0f11ef29e7161955cb8a6e346
tmxhsk99 <tmxhsk99@naver.com>
2023-05-11 14:11:52 +0900

    feat: 일기 State 추가

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import {Route, Routes} from "react-router-dom";
+import {useReducer} from "react";
+
+function reducer(state, action) {
+    return state;
+}
 
 function App() {
+    const [data, dispatch] = useReducer(reducer, []);
+
     return (
         <div className="App">
             <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/new" element={<New/>} />
-                <Route path="/diary/:id" element={<Diary/>} />
-                <Route path="/edit" element={<Edit/>} />
+                <Route path="/" element={<Home/>}/>
+                <Route path="/new" element={<New/>}/>
+                <Route path="/diary/:id" element={<Diary/>}/>
+                <Route path="/edit" element={<Edit/>}/>
             </Routes>
         </div>
     );
