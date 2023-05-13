882001700f6b6ad9cb73835ae34c18c0070544bc
tmxhsk99 <tmxhsk99@naver.com>
2023-05-13 11:53:30 +0900

    feat : 일기 상세 페이지 헤더 부분 구현

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,14 +1,39 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import useDiary from "../hooks/useDiary";
+import Header from "../component/Header";
+import {getFormattedDate} from "../util";
+import Button from "../component/Button";
 
-const Diary = () =>{
+const Diary = () => {
     const {id} = useParams();
-    return (
-        <div>
-            <div>id : {id}</div>
-            <div>Diary 페이지 입니다</div>
-        </div>
+    const data = useDiary(id);
 
-    )
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    }
+    const goEdit = () => {
+        navigate(`/edit/${id}`);
+    }
+
+    if (!data) {
+        return <div>일기를 불러오고 있습니다....</div>
+    } else {
+        const {date, emotionId, content} = data;
+        const title = `${getFormattedDate(new Date(Number(date)))} 기록`
+        return (
+            <div>
+                <Header
+                    title={title}
+                    leftChild={<Button text={"< 뒤로 가기"} onClick={goBack}/>}
+                    rigthChild={<Button text={"수정하기"} onClick={goEdit}/>}
+                />
+                <div>data : {id}</div>
+                < div> Diary 페이지 입니다</div>
+            </div>
+        );
+    }
+
 }
 
 export default Diary;
