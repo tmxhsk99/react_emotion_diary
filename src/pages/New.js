26914c161df91348f69580b3ef5f4015499dda0d
tmxhsk99 <tmxhsk99@naver.com>
2023-05-15 10:41:19 +0900

    feat : 일기 작성 페이지 기능및 컴포넌트 개발

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,4 +1,31 @@
+import {useNavigate} from "react-router-dom";
+import Header from "../component/Header";
+import Button from "../component/Button";
+import Editor from "../component/Editor";
+import {useContext} from "react";
+import {DiaryDispatchContext} from "../App";
+
 const New = () => {
-    return <div> 뉴 페이지 입니다</div>;
-}
+    const {onCreate} = useContext(DiaryDispatchContext);
+    const navigate = useNavigate();
+    const goBaok = () => {
+        navigate(-1);
+    }
+    const onSubmit = (data) => {
+        console.log(data);
+        const {date, content, emotionId} = data;
+        const calculableDate = new Date(date);
+        onCreate(calculableDate, content, emotionId);
+        navigate("/",{replace: true});
+    }
+    return (
+        <div>
+            <Header
+                title={"새 일기 쓰기"}
+                leftChild={<Button text={"< 뒤로가기"} onClick={goBaok}/>}
+            />
+            <Editor onSubmit={onSubmit}/>
+        </div>
+    );
+};
 export default New;
